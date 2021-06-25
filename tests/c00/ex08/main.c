dcefcc3bcb539d705e60d87efca56d373bf1c4a5
alaamimi <aku-no-hana@outlook.fr>
2021-06-25 03:38:28 +0100

    printcombn tester implemented

--- tests/c00/ex08/main.c
+++ tests/c00/ex08/main.c
@@ -6,14 +6,31 @@
 /*   By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 19:19:53 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/06/21 19:19:56 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/06/25 02:20:54 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/user_functions.h"
 
-int		main(void)
+
+int main(void)
 {
 	ft_print_combn(1);
-	return (0);
+	write(1, "\n", 1);
+	ft_print_combn(2);
+	write(1, "\n", 1);
+	ft_print_combn(3);
+	write(1, "\n", 1);
+	ft_print_combn(4);
+	write(1, "\n", 1);
+	ft_print_combn(5);
+	write(1, "\n", 1);
+	ft_print_combn(6);
+	write(1, "\n", 1);
+	ft_print_combn(7);
+	write(1, "\n", 1);
+	ft_print_combn(8);
+	write(1, "\n", 1);
+	ft_print_combn(9);
+	write(1, "\n", 1);
 }
